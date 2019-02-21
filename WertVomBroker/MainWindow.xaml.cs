509ff0ad1271b0df20beb804c4380a67d03b93c7
Alexander THURAU <thuraua@edu.htl-villach.at>
2019-02-21 13:24:08 +0100

    official solution

--- WertVomBroker/MainWindow.xaml.cs
+++ WertVomBroker/MainWindow.xaml.cs
@@ -2,37 +2,17 @@
 using LiveCharts.Wpf;
 using System;
 using System.Text;
-using System.Timers;
 using System.Windows;
 using uPLibrary.Networking.M2Mqtt;
 using uPLibrary.Networking.M2Mqtt.Messages;
 
 namespace WertVomBroker
 {
-    /// <summary>
-    /// Interaktionslogik für MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         private MqttClient client;
         private string clientId;
-        private static System.Timers.Timer aTimer;
-        private void SetTimer()
-        {
-            aTimer = new Timer(250);
-            aTimer.Elapsed += OnTimedEvent;
-            aTimer.AutoReset = true;
-            aTimer.Enabled = true;
-        }
-
-        private void OnTimedEvent(object sender, ElapsedEventArgs e)
-        {
-            double random = new Random().NextDouble() * 10;
-            Dispatcher.Invoke(delegate { textBox.Text = random.ToString(); });
-            if (Values.Count >= 10)
-                Values.RemoveAt(0);
-            Values.Add(random);
-        }
+        private static int maxNumberOfValuesDisplayed = 30;
         public ChartValues<double> Values { get; set; }
         public MainWindow()
         {
@@ -43,8 +23,6 @@ namespace WertVomBroker
             clientId = Guid.NewGuid().ToString();
             client.Connect(clientId);
             client.Subscribe(new string[] { "villach/4BHIF/Temperatur" }, new byte[] { 2 });
-            textBox.Text = "";
-            SetTimer();
         }
         private void DoChartStuff()
         {
@@ -66,24 +44,17 @@ namespace WertVomBroker
         }
         private void ButtonAus_Click(object sender, RoutedEventArgs e)
         {
-            //if (Values.Count >= 10)
-            //    Values.RemoveAt(0);
-            //Values.Add(new Random().NextDouble()*10);
             client.Publish("villach/4BHIF/Schülername/Luefter", Encoding.ASCII.GetBytes("off"));
         }
         private void ButtonEin_Click(object sender, RoutedEventArgs e)
         {
-            //if (Values.Count >= 10)
-            //    Values.RemoveAt(0);
-            //Values.Add(new Random().NextDouble() * 10);
             client.Publish("villach/4BHIF/Schülername/Luefter", Encoding.ASCII.GetBytes("on"));
-
         }
         private void MqttMsgReceived(object sender, MqttMsgPublishEventArgs e)
         {
             string ReceivedMessage = Encoding.UTF8.GetString(e.Message);
             double newChartValue = Convert.ToDouble(ReceivedMessage);
-            if (Values.Count >= 10)
+            if (Values.Count >= maxNumberOfValuesDisplayed) //es werden nur die letzten paar Werte angezeigt
                 Values.RemoveAt(0);
             Values.Add(newChartValue);
             Dispatcher.Invoke(delegate { textBox.Text = ReceivedMessage; });
